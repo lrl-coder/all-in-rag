493e4f6214aebdde0656de6cbf821a68dcc97c61
lrl-coder <1559589961@qq.com>
2026-05-23 11:22:33 +0800

    分块

--- code/C2/03_recursive_character_splitter.py
+++ code/C2/03_recursive_character_splitter.py
@@ -8,7 +8,7 @@ text_splitter = RecursiveCharacterTextSplitter(
     # 针对中英文混合文本，定义一个更全面的分隔符列表
     separators=["\n\n", "\n", "。", "，", " ", ""], # 按顺序尝试分割
     chunk_size=200,
-    chunk_overlap=10
+    chunk_overlap=40
 )
 
 chunks = text_splitter.split_documents(docs)
